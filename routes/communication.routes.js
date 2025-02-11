a8361188224244d6cce6dffda6f3fea1e590e617
Sachin <sachin.rangar8606@gmail.com>
2025-02-11 21:30:37 +0530

    media upload size 50mb

--- routes/communication.routes.js
+++ routes/communication.routes.js
@@ -5,7 +5,12 @@ const { authenticate } = require('../middlewares/auth.middleware');
 const { s3Upload } = require('../utils/s3Upload.utils');
 
 const router = express.Router();
-const upload = multer(); 
+const upload = multer({
+  limits: {
+    fileSize: 50 * 1024 * 1024
+  },
+});
+
 router.post('/create',authenticate, createMessage);
 router.get('/between/:userId1/:userRole1/:userId2/:userRole2',authenticate, getMessagesBetweenUsers);
 router.get('/user/:userId/:userRole',authenticate, getMessagesForUser);
