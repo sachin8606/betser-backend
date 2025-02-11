ab2df5de4a34e6ae5c674f1f07b7bebca1ede5f0
Sachin <sachin.rangar8606@gmail.com>
2025-02-11 23:24:21 +0530

    media upload size increased

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
 
 // Routes
 const userRoutes = require('./routes/user.routes');
